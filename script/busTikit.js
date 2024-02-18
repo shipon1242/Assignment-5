8fc40e28f3c3277008c00206af8fe743b04e414c
shipon1242 <hossainmohammadshipon@gamil.com>
2024-02-18 19:45:49 +0600

    available and selected seat count

--- script/busTikit.js
+++ script/busTikit.js
@@ -26,13 +26,16 @@ function seatColor(){
     // getseatsId()
     let availableSeat=40;
     let selectSeat=0;
+    let selectSeatsElement=document.getElementById('select-seat')
+    
+    
    let AvailableSeatsElement  = document.getElementById('available-seats')
-let AvailableSeats = AvailableSeatsElement.innerText
-let seatscount=parseInt(AvailableSeats)
+
 
 
     
     const seatsId=['A1','A2','A3','A4','B1','B2','B3','B4','C1','C2','C3','C4','D1','D2','D3','D4','E1','E2','E3','E4','F1','F2','F3','F4','G1','G2','G3','G4','H1','H2','H3','H4','I1','I2','I3','I4','J1','J2','J3','J4']
+    
 for(const seatId of seatsId ){
     const seatElement=document.getElementById(seatId)
     
@@ -41,13 +44,14 @@ for(const seatId of seatsId ){
         event.target.style.backgroundColor='green'
         selectSeat=selectSeat+1;
         availableSeat=availableSeat-1;
-        console.log(availableSeat,selectSeat, seatscount)
-        seatscount=availableSeat
-        // console.log(AvailableSeats,selectSeat)
+       
+        AvailableSeatsElement.innerText=availableSeat
         
+        selectSeatsElement.innerText=selectSeat
     })
     
    }
+  
 //    const AvailableSeatsElement  = document.getElementById('available-seats')
 //     const AvailableSeats= AvailableSeatsElement.innerText
 //     console.log(AvailableSeats)
