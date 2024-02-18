1d0950a8ffc1ea7861443d392af3d359bfe4ec1b
shipon1242 <hossainmohammadshipon@gamil.com>
2024-02-18 17:54:32 +0600

    available seat half complite

--- script/busTikit.js
+++ script/busTikit.js
@@ -26,6 +26,12 @@ function seatColor(){
     // getseatsId()
     let availableSeat=40;
     let selectSeat=0;
+   let AvailableSeatsElement  = document.getElementById('available-seats')
+let AvailableSeats = AvailableSeatsElement.innerText
+let seatscount=parseInt(AvailableSeats)
+
+
+    
     const seatsId=['A1','A2','A3','A4','B1','B2','B3','B4','C1','C2','C3','C4','D1','D2','D3','D4','E1','E2','E3','E4','F1','F2','F3','F4','G1','G2','G3','G4','H1','H2','H3','H4','I1','I2','I3','I4','J1','J2','J3','J4']
 for(const seatId of seatsId ){
     const seatElement=document.getElementById(seatId)
@@ -34,14 +40,26 @@ for(const seatId of seatsId ){
     seatElement.addEventListener('click',function(event){
         event.target.style.backgroundColor='green'
         selectSeat=selectSeat+1;
+        availableSeat=availableSeat-1;
+        console.log(availableSeat,selectSeat, seatscount)
+        seatscount=availableSeat
+        // console.log(AvailableSeats,selectSeat)
         
     })
-   }
-
     
+   }
+//    const AvailableSeatsElement  = document.getElementById('available-seats')
+//     const AvailableSeats= AvailableSeatsElement.innerText
+//     console.log(AvailableSeats)
     
 }
 // setBackgroundColor('A1')
+
+
+// availbe seat id check
+
+
+
 
 
 
